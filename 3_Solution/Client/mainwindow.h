2eb3a6cba7f2ba2ad01b68728e7e48645c579415
PoplauschiAndrei <126865105+PoplauschiAndrei@users.noreply.github.com>
2023-05-26 11:08:18 +0300

    Create mainwindow.h

---
+++ 3_Solution/Client/mainwindow.h
@@ -0,0 +1,39 @@
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <second_page.h>
+#include <create_account.h>
+#include <QtNetwork//qtcpsocket.h>
+#include <qtimer.h>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void read_from_server();
+
+private:
+    Ui::MainWindow *ui;
+    Second_Page * secPage;
+    Create_Account * accPage;
+    QTcpSocket *socket;
+    QTimer * timer;
+};
+
+#endif // MAINWINDOW_H
