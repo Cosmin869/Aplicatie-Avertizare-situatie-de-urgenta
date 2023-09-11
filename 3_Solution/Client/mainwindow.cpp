a1a7963bb79f5a491fd582dbcc86f2cbd5ef60cd
PoplauschiAndrei <126865105+PoplauschiAndrei@users.noreply.github.com>
2023-09-11 17:21:19 +0300

    mainwindow.cpp

--- 3_Solution/Client/mainwindow.cpp
+++ 3_Solution/Client/mainwindow.cpp
@@ -1,4 +1,3 @@
-
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
@@ -10,7 +9,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     this->socket = new QTcpSocket();
-    socket->connectToHost("192.168.43.7", 54001);
+    socket->connectToHost("192.168.43.74", 54001);
     socket->open(QIODevice::ReadWrite);
     this->timer = new QTimer(this);
     connect(this->socket, SIGNAL(readyRead()), this, SLOT(read_from_server()));
@@ -23,20 +22,28 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::read_from_server() {
-
-    if(this->socket != nullptr && this->socket->isOpen()) {
+void MainWindow::read_from_server()
+{
+    if(this->socket != nullptr && this->socket->isOpen())
+    {
         QByteArray response = this->socket->readAll();
         qDebug() << response;
         QString response_str = QString::fromStdString(response.toStdString());
         if (response_str == "You're logged in!")
         {
-            hide();
+            this->hide();
             secPage = new Second_Page(this, this->socket);
-            secPage -> show();
+            secPage->setModal(true);
+            secPage->exec();
+        }
+        else if(response_str == "You're Admin!")
+        {
+            this->hide();
+            Administrator adm(this, this->socket);
+            adm.setModal(true);
+            adm.exec();
         }
     }
-
 }
 
 void MainWindow::on_pushButton_clicked()
@@ -46,12 +53,7 @@ void MainWindow::on_pushButton_clicked()
     QString username = ui -> userName ->text();
     QString password = ui -> userPass ->text();
 
-
-//
-//    else
-//    {
-//        QMessageBox::warning(this,"Login","Username and password are not correct");
-//    }
+    //QString verif = xorEncrypt(username,'x');
 
     QString to_send;
     to_send = "0";
@@ -67,7 +69,7 @@ void MainWindow::on_pushButton_clicked()
 }
 void MainWindow::on_pushButton_2_clicked()
 {
-    //hide();
+    hide();
     //QString protocol = "1";
     //socket->write(protocol.toStdString().c_str());
     //this->socket->flush();
