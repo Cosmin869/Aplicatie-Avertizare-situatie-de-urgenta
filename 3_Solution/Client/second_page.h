4c48a7ba21e55bbc56c0fc017d6f237655e0174c
PoplauschiAndrei <126865105+PoplauschiAndrei@users.noreply.github.com>
2023-09-11 17:23:23 +0300

    second_page.h

--- 3_Solution/Client/second_page.h
+++ 3_Solution/Client/second_page.h
@@ -3,6 +3,20 @@
 
 #include <QDialog>
 #include <QtNetwork/QTcpSocket>
+#include <QtNetwork/QNetworkReply>
+#include <qtimer.h>
+
+class MainWindow;
+
+struct CountryInfo {
+    int id;
+    QString country_name;
+};
+
+struct DisasterInfo {
+    int id;
+    QString disaster_name;
+};
 
 namespace Ui {
 class Second_Page;
@@ -15,15 +29,28 @@ class Second_Page : public QDialog
 public:
     explicit Second_Page(QWidget *parent = nullptr, QTcpSocket* socket = nullptr);
     ~Second_Page();
+    QVector<CountryInfo> countryVector;
+    QVector<DisasterInfo> disasterVector;
 
 private slots:
     void on_pushButton_clicked();
 
-    //void on_textBrowser_historyChanged();
+    void onNetworkReply(QNetworkReply *reply);
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void listen_for_alerts();
+
+    void on_pushButton_4_clicked();
 
 private:
     Ui::Second_Page *ui;
     QTcpSocket *socket;
+    MainWindow * mw;
+    QTimer * timer;
+    QString initialApiUrl = "https://api.reliefweb.int/v1/disasters?appname=rwint-user-0&profile=list&preset=latest&slim=1";
 };
 
 #endif // SECOND_PAGE_H
