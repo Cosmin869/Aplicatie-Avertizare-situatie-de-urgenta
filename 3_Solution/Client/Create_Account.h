c975fc46238750a256e9c32933110fa005d54a7d
PoplauschiAndrei <126865105+PoplauschiAndrei@users.noreply.github.com>
2023-09-11 17:19:38 +0300

    Create_Account.h

--- 3_Solution/Client/Create_Account.h
+++ 3_Solution/Client/Create_Account.h
@@ -1,10 +1,11 @@
-
 #ifndef CREATE_ACCOUNT_H
 #define CREATE_ACCOUNT_H
 
 #include <QDialog>
 #include <QtNetwork//qtcpsocket.h>
 #include <qtimer.h>
+
+class MainWindow;
 
 namespace Ui {
 class Create_Account;
@@ -18,15 +19,16 @@ public:
     explicit Create_Account(QWidget *parent = nullptr);
     ~Create_Account();
 
-    //metoda pentru creare de cont
-
 private slots:
     void on_pushButton_clicked();
+
+    void on_back_button_clicked();
 
 private:
     Ui::Create_Account *ui;
     QTcpSocket *socket;
     QTimer * timer;
+    MainWindow * mw2;
 };
 
 #endif // CREATE_ACCOUNT_H
